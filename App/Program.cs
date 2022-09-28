6e5a1e8038748138a9df59a3b0de0f10dfff5926
Freddy Söderberg <freddy.soderberg@edu.edugrade.se>
2022-09-28 15:21:59 +0200

    Added NuGets for appcenter.ms

--- App/Program.cs
+++ App/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace App
 {
@@ -17,6 +20,8 @@ namespace App
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AppCenter.Start("{2735ff41 - 0403 - 4fe3 - 9e29 - ff4d05fc6449}",
+                  typeof(Analytics), typeof(Crashes));
             Application.Run(new GUI(new Calculator()));
         }
     }
