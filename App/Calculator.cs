56374d424e4b32507716cf8d9a1b30b7e1d5dd96
Robin Hörnkvist <robin.hornkvist@edu.edugrade.se>
2022-09-28 14:43:38 +0200

    comment to trigger release pipeline

--- App/Calculator.cs
+++ App/Calculator.cs
@@ -7,6 +7,7 @@ namespace App
 {
     public class Calculator : ICalculator
     {
+        // comment
         public double Addition(double valOne, double valTwo)
             => valOne + valTwo;
     }
