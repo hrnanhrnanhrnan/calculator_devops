b6280cff331e583d0cc4909ce4ad75316dad2dd8
Freddy Söderberg <freddy.soderberg@edu.edugrade.se>
2022-09-29 15:47:28 +0200

    Testing release to Github

--- App/Calculator.cs
+++ App/Calculator.cs
@@ -7,7 +7,7 @@ namespace App
 {
     public class Calculator : ICalculator
     {
-        //Freddu wuz her
+        //Freddu wuz her 2
         public double Addition(double valOne, double valTwo)
             => valOne + valTwo;
     }
