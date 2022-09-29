9e3d6da3927210cf47d2be93f2c6952ea469e19d
Freddy Söderberg <freddy.soderberg@edu.edugrade.se>
2022-09-29 15:29:24 +0200

    Testing release through pipeline

--- App/Calculator.cs
+++ App/Calculator.cs
@@ -7,6 +7,7 @@ namespace App
 {
     public class Calculator : ICalculator
     {
+        //Freddu wuz her
         public double Addition(double valOne, double valTwo)
             => valOne + valTwo;
     }
