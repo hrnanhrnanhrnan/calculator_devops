eb2ebfb44f79b5728bc4b5ebd303db9887904b4c
Mike Jones <kiamel76@gmail.com>
2022-10-04 13:47:35 +0200

    del comment

--- App/Calculator.cs
+++ App/Calculator.cs
@@ -8,7 +8,7 @@ namespace App
     public class Calculator : ICalculator
     {
         //Freddy wuz her 2
-        // ME2
+      
         public double Addition(double valOne, double valTwo)
             => valOne + valTwo;
 
